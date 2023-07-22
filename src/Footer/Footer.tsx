c060b656969329e668eaa096517bb0f60bf56bf7
Angelo Dell Acqua <angelo@angedell.it>
2023-07-22 15:37:23 -0400

    Footer reworked with new logo

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,8 +1,11 @@
+import { ReactComponent as BufoLogo } from './BufoOs.svg';
+
 function Footer() {
   return (
     <div className="Footer">
-      <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+      <footer className="footer items-center p-4 blue-gradient text-white">
         <div className="items-center grid-flow-col">
+          <BufoLogo className="fill-white h-10 w-10" />
           <p>Copyright © 2023 - BufoOS - All right reserved</p>
         </div>
       </footer>
