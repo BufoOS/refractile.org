8fd335327fe957c6e49b9d6aa0facd2fa64c87c6
Angelo Dell Acqua <angelo@angedell.it>
2023-07-26 19:41:00 -0400

    prism types pkg install

--- src/PackageReadme/PackageReadme.tsx
+++ src/PackageReadme/PackageReadme.tsx
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from "react";
-import Markdown from "markdown-to-jsx";
-const README = require('../refractile/README.md')
+import React, { useState, useEffect } from 'react';
+import Markdown from 'markdown-to-jsx';
+import Prism from 'prismjs';
+const README = require('../refractile/README.md');
 
 function PackageReadme(props: any) {
-  const [mdText, setMdText] = useState('')
+  const [mdText, setMdText] = useState('');
 
   useEffect(() => {
-    fetchMarkdown()
-  }, [])
+    fetchMarkdown();
+  }, []);
 
   async function fetchMarkdown() {
-    const response = await fetch(README)
-    const data = await response.text()
-    setMdText(data)
+    const response = await fetch(README);
+    const data = await response.text();
+    setMdText(data);
   }
 
   return (
     <div id="readme">
       <Markdown children={mdText} options={{}} />
     </div>
-  )
+  );
 }
 
-export default PackageReadme
+export default PackageReadme;
